63c4e6532f6d22e2f1632509707186801ae93c02
xerx593 <alexlutz79@gmail.com>
2019-02-05 23:05:24 +0100

    added mockbean sample

--- src/test/java/com/stackoverflow/soq54538520/MyControllerMockBeanTest.java
+++ src/test/java/com/stackoverflow/soq54538520/MyControllerMockBeanTest.java
@@ -1,21 +1,16 @@
 package com.stackoverflow.soq54538520;
 
-import com.stackoverflow.soq54538520.MyControllerTest.TestConfig;
 import com.stackoverflow.soq54538520.service.ServiceB;
 import static org.hamcrest.CoreMatchers.equalTo;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -25,21 +20,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
-@ContextConfiguration(classes = {TestConfig.class, Application.class})
-public class MyControllerTest {
+public class MyControllerMockBeanTest {
 
-    @Profile("test")
-    @Configuration
-    static class TestConfig {
+    @MockBean
+    private ServiceB mockB;
 
-        @Bean
-        @Primary
-        public ServiceB mockB() {
-            ServiceB mockService = Mockito.mock(ServiceB.class);
-            Mockito.when(mockService.greeting()).thenReturn("I am mock Service B!");
-            return mockService;
-        }
+    @Before
+    public void setup() {
+        Mockito.when(mockB.greeting()).thenReturn("I am mock Service B!");
     }
     @Autowired
     private MockMvc mvc;
@@ -48,12 +36,13 @@ public class MyControllerTest {
     public void testGreeting() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
-                //.andExpect(content().string(equalTo("A response: I am real ServiceA!, B response: I am real ServiceB!")));
+        //.andExpect(content().string(equalTo("A response: I am real ServiceA!, B response: I am real ServiceB!")));
     }
+
     @Test
     public void testGreetingMock() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("A response: I am real ServiceA!, B response: I am Mock Service B!")));
+                .andExpect(content().string(equalTo("A response: I am real ServiceA!, B response: I am mock Service B!")));
     }
 }
---
+++ src/test/java/com/stackoverflow/soq54538520/MyControllerProfileTest.java
@@ -0,0 +1,59 @@
+package com.stackoverflow.soq54538520;
+
+import com.stackoverflow.soq54538520.MyControllerProfileTest.TestConfig;
+import com.stackoverflow.soq54538520.service.ServiceB;
+import static org.hamcrest.CoreMatchers.equalTo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@ContextConfiguration(classes = {TestConfig.class, Application.class})
+public class MyControllerProfileTest {
+
+    @Profile("test")
+    @Configuration
+    static class TestConfig {
+
+        @Bean
+        @Primary
+        public ServiceB mockB() {
+            ServiceB mockService = Mockito.mock(ServiceB.class);
+            Mockito.when(mockService.greeting()).thenReturn("I am Mock Service B!");
+            return mockService;
+        }
+    }
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testGreeting() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+                //.andExpect(content().string(equalTo("A response: I am real ServiceA!, B response: I am real ServiceB!")));
+    }
+    @Test
+    public void testGreetingMock() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("A response: I am real ServiceA!, B response: I am Mock Service B!")));
+    }
+}
